d523d89f042a9717b4255b7c3ed16a7d85e48f7d
NikSakh <NikSakharow@gmail.com>
2026-04-30 19:50:31 +0300

    BCORE-16.1

--- src/main/java/ru/mentee/power/crm/spring/controller/LeadController.java
+++ src/main/java/ru/mentee/power/crm/spring/controller/LeadController.java
@@ -1,11 +1,11 @@
 package ru.mentee.power.crm.spring.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.mentee.power.crm.model.LeadDto;
 import ru.mentee.power.crm.service.LeadService;
-
-import java.util.List;
 
 @RestController
 public class LeadController {
