4308229f3be57297186a9e798bd2c49b736f4b8b
NikSakh <NikSakharow@gmail.com>
2026-05-05 00:29:27 +0300

    BCORE-19

---
+++ src/test/java/ru/mentee/power/crm/spring/LeadServiceBeanTest.java
@@ -0,0 +1,35 @@
+package ru.mentee.power.crm.spring;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import ru.mentee.power.crm.repository.LeadRepository;
+import ru.mentee.power.crm.service.LeadService;
+
+@SpringBootTest
+class LeadServiceBeanTest {
+
+  @Autowired
+  private ApplicationContext context;
+
+  @Test
+  void shouldCreateLeadServiceBean() {
+    LeadService service = context.getBean(LeadService.class);
+    assertThat(service).isNotNull();
+  }
+
+  @Test
+  void shouldCreateLeadRepositoryBean() {
+    LeadRepository repo = context.getBean(LeadRepository.class);
+    assertThat(repo).isNotNull();
+  }
+
+  @Test
+  void shouldInjectLeadRepositoryIntoService() {
+    LeadService service = context.getBean(LeadService.class);
+    assertThat(service.findAll()).isEmpty();
+  }
+}
